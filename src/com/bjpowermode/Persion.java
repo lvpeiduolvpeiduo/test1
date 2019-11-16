3b0fa1e4f033e72f63a0df00e4f4d0311f3e183a
zs <zs@bjpowernode.com>
2019-11-16 16:15:47 +0800

    初始导入

---
+++ src/com/bjpowermode/Persion.java
@@ -0,0 +1,17 @@
+package com.bjpowermode;
+
+/**
+ * className Persion
+ * package com.bjpowermode
+ * Description:
+ *
+ * @date:2019/11/1616:09
+ * @AUTHOR 16354
+ **//*
+ *Author 吕
+ *2019/11/16
+ */
+public class Persion {
+    private String age;
+
+}
